1d603b0bd2f6ab0c299daa18e08bdc6d087e45e2
jumaboyevaf267-source <jumaboyevaf267@gmail.com>
2026-07-18 10:55:05 +0500

    Create Config class to manage environment variables
    Add configuration class to load environment variables for the bot.

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    TELEGRAM_TOKEN = os.getenv("8859112289:AAFfySswTXD2bX9eX08kshjCOqgFrQ0gl3M")
+    GEMINI_API_KEY = os.getenv("AQ.Ab8RN6J9tkop9yvPLbYnIFStpcwoi50nqZ9Wtq1xs-sdylyPxg")
+    BOT_NAME = os.getenv("BOT_NAME", "Learning Korean AI v2")
+    ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+    DATABASE_PATH = os.getenv("DATABASE_PATH", "src/database/korean_bot.db")
+    PORT = int(os.getenv("PORT", 10000))
+    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+
+    @classmethod
+    def validate(cls):
+        if not cls.TELEGRAM_TOKEN or not cls.GEMINI_API_KEY:
+            raise ValueError("XATOLIK: .env faylida muhim API kalitlar yetishmayapti!")
+
+Config.validate()
